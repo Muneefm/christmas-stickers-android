01a37ff5c94b3c7eec6c0e48c0ddc707008e82a7
Muneef M <mailmuneef@gmail.com>
2018-11-25 15:57:48 +0530

    first commit

---
+++ app/src/main/java/com/example/samplestickerapp/StickerApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.example.samplestickerapp;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.google.android.gms.ads.MobileAds;
+
+public class StickerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+    }
+}
